5904f82f5e3f38e88d957d182629aa17f2e20c46
magnusknutas <magnus@thefarm.se>
2016-01-03 13:52:39 +0100

    Made testpypi so far

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from distutils.core import setup
+
+setup(
+        name='pyswedbank',
+        packages=['pyswedbank'],
+        version='0.0.1',
+        description='A python wrapper for swedbank api.',
+        author='Magnus Knutas',
+        author_email='magnusknutas@gmail.com',
+        url='https://github.com/spaam/swedbank-cli',
+        keywords=['banking', 'api', 'wrapper'],  # arbitrary keywords
+        classifiers=[],
+)
